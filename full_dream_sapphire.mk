9796dc8819beb85a2f5312f59baef2e764c56438
YangJie <yjsss75@gmail.com>
2011-02-20 11:12:32 +0800

    fix opengl

--- full_dream_sapphire.mk
+++ full_dream_sapphire.mk
@@ -113,7 +113,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=65536
+    ro.opengles.version=65537
 
 # media configuration xml file
 PRODUCT_COPY_FILES += \
